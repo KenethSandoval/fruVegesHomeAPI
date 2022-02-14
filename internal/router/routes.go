ab9528ea17b1134a3e190830d26f236525908079
KenethSandoval <kenethra.74@gmail.com>
2022-02-14 12:04:54 -0600

    feat: add middlewares

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,52 +1,19 @@
 package router
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/KenethSandoval/fvexpress/internal/auth"
 	"github.com/KenethSandoval/fvexpress/internal/router/orders"
 	"github.com/KenethSandoval/fvexpress/internal/router/products"
+	"github.com/KenethSandoval/fvexpress/pkg/middleware"
 	"github.com/gorilla/mux"
 )
 
-type authenticationMiddleware struct {
-	tokenUsers map[string]string
-}
-
-// Initialize it somewhere
-func (amw *authenticationMiddleware) Populate() {
-	amw.tokenUsers = make(map[string]string)
-	amw.tokenUsers["00000000"] = "user0"
-	amw.tokenUsers["aaaaaaaa"] = "userA"
-	amw.tokenUsers["05f717e5"] = "randomUser"
-	amw.tokenUsers["deadbeef"] = "user0"
-}
-
-// Middleware function, which will be called for each request
-func (amw *authenticationMiddleware) Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("X-Session-Token")
-
-		if user, found := amw.tokenUsers[token]; found {
-			// We found the token in our map
-			log.Printf("Authenticated user %s\n", user)
-			// Pass down the request to the next middleware (or final handler)
-			next.ServeHTTP(w, r)
-		} else {
-			// Write an error and stop the handler chain
-			http.Error(w, "Forbidden", http.StatusForbidden)
-		}
-	})
-}
-
 func InitRouter() *mux.Router {
-	amw := authenticationMiddleware{}
-	amw.Populate()
 	router := mux.NewRouter()
 	router = router.PathPrefix("/api").Subrouter()
+	router.Use(middleware.LoggingMiddleware)
 	privateR := router.NewRoute().Subrouter()
-	privateR.Use(amw.Middleware)
+	privateR.Use(middleware.ValidateMiddleware)
 
 	// Products
 	privateR.HandleFunc("/products", products.GetProducts).Methods("GET")
